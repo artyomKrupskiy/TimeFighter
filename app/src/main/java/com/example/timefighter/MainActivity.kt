e9a1f3478ff1349a3398debafb8e54594ba0d97b
Artyom Krupskiy <kru5a777@gmail.com>
2022-03-23 19:25:53 +0300

    Test initial commit

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+            Initial commit...
+         */
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
